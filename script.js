76a8a02c8c405ee02cdb7611f004f2fdecbd2867
zarif-tajwar <zariftjwr.catalyst@gmail.com>
2023-09-21 00:13:54 +0600

    pre finalize

--- script.js
+++ script.js
@@ -1,58 +1,90 @@
 "use strict";
 
-const revealTextParent = document.querySelector(".reveal-text-parent");
-const btn = document.querySelector(".btn");
+const scrollThreshold = 0.8;
+const defaultDurationInMs = 700;
+const defaultEasing = "ease";
 
-let isClicked = false;
-
-const wordsArray = revealTextParent.textContent
-  .split(/\s+/)
-  .filter((word) => word != "");
-
-const wordsArrayWithNewLine = revealTextParent.textContent.split();
-
-revealTextParent.textContent = "";
-
-async function sleep(delayInSeconds) {
-  return new Promise((res) => setTimeout(res, delayInSeconds * 1000));
+async function sleep(delayInMS) {
+  return new Promise((res) => setTimeout(res, delayInMS));
 }
 
-wordsArray.forEach((word) => {
-  revealTextParent.insertAdjacentHTML(
-    "beforeend",
-    `<span class='reveal-text-container'><span class='reveal-text reveal-text--hidden'>${word}&nbsp;</span></span>`
+const revealTextParents = document.querySelectorAll(".reveal-text-parent");
+
+revealTextParents.forEach((revealTextParent) => {
+  const wordsArray = revealTextParent.textContent
+    .split(/\s+/)
+    .filter((word) => word != "");
+
+  revealTextParent.textContent = "";
+
+  wordsArray.forEach((word) => {
+    revealTextParent.insertAdjacentHTML(
+      "beforeend",
+      `<span class='reveal-text-container'><span class='reveal-text reveal-text--hidden'>${word}&nbsp;</span></span>`
+    );
+  });
+});
+
+function animateText(revealTextParent) {
+  const duration =
+    Number.parseInt(revealTextParent.dataset.durationInMs) ||
+    defaultDurationInMs;
+  const ease = revealTextParent.dataset.durationInMs || defaultEasing;
+
+  const lines = [];
+
+  const wordElemets = Array.from(
+    revealTextParent.querySelectorAll(".reveal-text")
   );
-});
 
-const lines = [];
-const wordElemets = Array.from(document.querySelectorAll(".reveal-text"));
-let currentLineGroup = [wordElemets[0]];
+  wordElemets.forEach((el) => {
+    el.style.transition = `all ${duration}ms ${ease}`;
+  });
 
-for (let i = 1; i < wordElemets.length; i++) {
-  if (
-    wordElemets[i].getBoundingClientRect().top ===
-    wordElemets[i - 1].getBoundingClientRect().top
-  ) {
-    currentLineGroup.push(wordElemets[i]);
-  } else {
-    lines.push(currentLineGroup);
-    currentLineGroup = [wordElemets[i]];
+  let currentLineGroup = [wordElemets[0]];
+
+  for (let i = 1; i < wordElemets.length; i++) {
+    if (
+      wordElemets[i].getBoundingClientRect().top ===
+      wordElemets[i - 1].getBoundingClientRect().top
+    ) {
+      currentLineGroup.push(wordElemets[i]);
+    } else {
+      lines.push(currentLineGroup);
+      currentLineGroup = [wordElemets[i]];
+    }
   }
+
+  lines.push(currentLineGroup);
+
+  lines.reverse();
+
+  (async () => {
+    for (let i = 0; i < lines.length; i++) {
+      if (i !== 0) {
+        await sleep(0.3);
+      }
+      lines[i].forEach((lineText) => {
+        lineText.classList.remove("reveal-text--hidden");
+      });
+    }
+  })();
 }
 
-lines.push(currentLineGroup);
-
-lines.reverse();
-
-btn.addEventListener("click", async () => {
-  for (let i = 0; i < lines.length; i++) {
-    if (i !== 0) {
-      await sleep(0.2);
-    }
-    lines[i].forEach((lineText) => {
-      lineText.classList.toggle("reveal-text--hidden");
+const textObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      console.log(entry);
+      const revealTextParent = entry.target;
+      if (entry.isIntersecting) {
+        animateText(revealTextParent);
+        textObserver.unobserve(entry.target);
+      }
     });
-  }
-});
+  },
+  { threshold: scrollThreshold }
+);
 
-const observer = new IntersectionObserver();
+revealTextParents.forEach((parent) => {
+  textObserver.observe(parent);
+});
