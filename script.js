1a29c7258f65c7dec5e99b22a61f2fe0f02938c7
zarif-tajwar <zariftjwr.catalyst@gmail.com>
2023-09-23 15:38:36 +0600

    add threshold and rootmargin control

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 
 /* Change the default values from here. These values apply for all elements by default.  */
 
-const scrollThreshold = 0.8;
+const defaultScrollThresholdInPercentage = 80;
+const defaultScrollViewportPercentage = 0;
 const defaultDurationInMs = 700;
 const defaultEasing = "ease";
 const defaultStaggerDelayInMS = 150;
@@ -16,6 +17,8 @@ control their animation individually.
 "data-duration-in-ms"
 "data-easing" 
 "data-stagger-delay-in-ms"
+"data-scroll-threshold-in-percentage" : between 0-100
+"data-scroll-viewport-percentage" : between 0-100
 
 */
 
@@ -99,19 +102,48 @@ function animateText(revealTextParent) {
   })();
 }
 
-const textObserver = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      const revealTextParent = entry.target;
-      if (entry.isIntersecting) {
-        animateText(revealTextParent);
-        textObserver.unobserve(entry.target);
-      }
-    });
-  },
-  { threshold: scrollThreshold }
-);
-
 revealTextParents.forEach((parent) => {
+  const parsedScrollThreshold = Number.parseInt(
+    parent.dataset.scrollThresholdInPercentage
+  );
+  const parsedScrollViewportPercentage = Number.parseInt(
+    parent.dataset.scrollViewportPercentage
+  );
+
+  const scrollThreshold =
+    (parsedScrollThreshold === 0 || parsedScrollThreshold
+      ? parsedScrollThreshold
+      : defaultScrollThresholdInPercentage) / 100;
+
+  let rootMargin = undefined;
+
+  let scrollViewportPercentage = defaultScrollViewportPercentage / 100;
+
+  if (parsedScrollViewportPercentage === 0 || parsedScrollViewportPercentage) {
+    scrollViewportPercentage = parsedScrollViewportPercentage / 100;
+  }
+  const bottomMargin =
+    window.innerHeight * scrollViewportPercentage -
+    parent.getBoundingClientRect().height;
+  if (bottomMargin > 0) {
+    rootMargin = `0px 0px -${bottomMargin}px 0px`;
+  }
+
+  const textObserver = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        const revealTextParent = entry.target;
+        if (entry.isIntersecting) {
+          animateText(revealTextParent);
+          textObserver.unobserve(entry.target);
+        }
+      });
+    },
+    {
+      threshold: scrollThreshold,
+      rootMargin,
+    }
+  );
+
   textObserver.observe(parent);
 });
